e076f29b8074e33b0c7ce2b41c763d37f0cf78e8
Ang56187 <ax54@live.com>
2020-12-21 16:53:52 +0800

    Second commit

--- src/reducers/CountReducer.js
+++ src/reducers/CountReducer.js
@@ -3,9 +3,9 @@ const INITIAL_STATE = {count:0};
 export default (state = INITIAL_STATE,action) => {
     switch(action.type){
         case 'increment':
-            return {count: state.count+1};
+            return {count: state.count+action.payload};
         case 'decrement':
-            return {count: state.count-1};
+            return {count: state.count-action.payload};
         default:
             return state;
     }
